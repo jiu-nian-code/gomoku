3a461972ca3c59020210ad9d7671200fe69a1c5f
jiu-nian-code <2136614575@qq.com>
2025-12-09 22:53:08 +0800

    12_9

--- soure/online.hpp
+++ soure/online.hpp
@@ -1,12 +1,14 @@
+#pragma once
 #include"websocketpp/server.hpp"
 #include"websocketpp/config/asio_no_tls.hpp"
 #include<unordered_map>
 #include<mutex>
 #include"util.hpp"
+#include"db.hpp"
 
 typedef websocketpp::server<websocketpp::config::asio> websocketsvr;
 
-class online_manager
+class Online_Manager
 {
     std::unordered_map<int, websocketsvr::connection_ptr> _game_room;
     std::unordered_map<int, websocketsvr::connection_ptr> _game_hall;
@@ -58,5 +60,53 @@ public:
             return false;
         }
         return true;
+    }
+};
+
+typedef enum { GAME_START, GAME_OVER } Game_Stu;
+class Room
+{
+    int _room_id; // 房间id
+    int _user_count; // 房间中的用户数量
+    int _white_id; // 白棋方用户id
+    int _black_id; // 黑棋方用户id
+    Online_Manager* _om; // 用户管理类
+    User_Table* _ut;
+    Game_Stu _gs;
+    std::vector<std::vector<int>> _chessboard; // 棋盘
+public:
+    Room(int room_id, Online_Manager* om, User_Table* ut) : 
+        _room_id(room_id), _user_count(0),
+        _white_id(-1), _black_id(-1),
+        _om(om), _ut(ut), _gs(GAME_START)
+    {
+        DBG_LOG("%d room create success.");
+    }
+
+    ~Room()
+    {
+        DBG_LOG("%d room destory success.");
+    }
+
+    void handle_chess() // 处理下棋操作
+    {
+
+    }
+
+    void handle_chat()
+    {
+
+    }
+
+    void handle_exit()
+    {
+
+    }
+
+    // 由大厅进入房间，用户已经准备就绪，所以不会有处理用户进入的函数
+
+    void handle_request(Json::Value req)
+    {
+
     }
 };
